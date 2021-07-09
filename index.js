72206d8dcc2574a7e6817b5bab3ee8abf3a002e4
Nurina <nurina.junus@gmail.com>
2021-07-08 21:39:59 -0600

    setup html, outline js

---
+++ index.js
@@ -0,0 +1,11 @@
+// fetch tcapData and save to const
+
+// use tcapData to populate districtDropdown with districts
+
+// add event listener to districtDropdown
+
+// add event listener to districtSearch
+
+// function to fill in scoreList/scoreTable
+
+// event listener to remove list or table elements
