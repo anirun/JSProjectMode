4fa684e3cce77231535420c6f1b7bae0ef39bd33
Nurina <nurina.junus@gmail.com>
2021-07-08 22:07:48 -0600

    structure TCAP score table, fetchData

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-// fetch tcapData and save to const
+// define API URL to fetch
+dataUrl = "https://data.colorado.gov/resource/pxq3-yhfb.json"
 
-// use tcapData to populate districtDropdown with districts
+/* create fetchData function to parse API JSON into a JS Object 
+that can later be manipulated with a chained .then() */
+let fetchData = function(){
+    return fetch(dataUrl)
+        .then(json => json.json())
+}
+
+// use tcapData to populate districtDropdown with district-name
 
 // add event listener to districtDropdown
 
 // add event listener to districtSearch
 
-// function to fill in scoreList/scoreTable
+// function to fill in tcap-scores table with district-name and district-avg
 
 // event listener to remove list or table elements
